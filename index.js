79576f647510fa8ea47cefd40ee771a1628d8408
Scott Weinstein <sweinstein27@gmail.com>
2018-10-29 13:48:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const logDriverNames = function (drivers) {
 const logDriversByHometown = function (drivers, hometown) {
   drivers.forEach(function (driver) {
     if (driver.hometown === hometown){
-    console.log(driver.hometown)
+    console.log(driver.name)
   }
   });
 };
